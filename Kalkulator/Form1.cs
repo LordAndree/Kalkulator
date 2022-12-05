df834ffc318a5e4cf8f0552471113e1ad6e1902c
asus <asus@RAPTORZ-PC>
2022-12-05 17:46:18 +0700

    Add project files.

---
+++ Kalkulator/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalkulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Hasil_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.handler += display;
+            form2.ShowDialog();
+        }
+
+        private void display(int nilai_a, int nilai_b, string operasi, int hasil, string symbol)
+        {
+            Hasil.Items.Add(string.Format("Hasil dari operasi {0} {1} {2} {3} = {4}", operasi, nilai_a, symbol, nilai_b, hasil));
+        }
+    }
+}
---
+++ Kalkulator/Form2.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalkulator
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            operasi();
+        }
+
+        public delegate void Form2ClickEventHandler(int nilai_a, int nilai_b, string operasi, int hasil, string symbol);
+        public event Form2ClickEventHandler handler;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int nilai_a = int.Parse(textBox1.Text);
+            int nilai_b = int.Parse(textBox2.Text);
+            int hasil = 0;
+            string operasi = "";
+            string symbol = "";
+            if (comboBox1.SelectedIndex == 0)
+            {
+                hasil = nilai_a + nilai_b;
+                operasi = "Penambahan";
+                symbol = "+";
+            }
+            else if(comboBox1.SelectedIndex == 1)
+            {
+                hasil = nilai_a - nilai_b;
+                operasi = "Pengurangan";
+                symbol = "-";
+            }
+            else if(comboBox1.SelectedIndex == 2)
+            {
+                hasil = nilai_a * nilai_b;
+                operasi = "Perkalian";
+                symbol = "*";
+            }
+            else if(comboBox1.SelectedIndex == 3)
+            {
+                hasil = nilai_a / nilai_b;
+                operasi = "Pembagian";
+                symbol = ":";
+            }
+            handler(nilai_a, nilai_b, operasi, hasil, symbol);
+            
+        }
+        private void operasi()
+        {
+            comboBox1.Items.Add("Penambahan");
+            comboBox1.Items.Add("Pengurangan");
+            comboBox1.Items.Add("Perkalian");
+            comboBox1.Items.Add("Pembagian");
+            comboBox1.SelectedIndex = 0;
+
+        }
+    }
+}
